6fb7c2d8fefd07cffcfb491a7fbf5256429b4aae
AND3SIL4 <devaul.fs@gmail.com>
2026-03-20 16:05:34 -0500

    update: create a new function to validate

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,15 @@
-from dataclasses import asdict, dataclass
-from json import dumps
+import unicodedata
 
 
-@dataclass
-class IdCardParts:
-    plan: str
-    contract: str
-    family: str
-    user: str
+def strip_acents(string: str) -> str:
+    """Strip accents from a string using Unicode normalization."""
+    # Normalize the string to NDF (Canolical Descomposition)
+    nfd_form = unicodedata.normalize("NFD", string)
+    # Filter out all combining characters (which have the 'mn' category)
+    striped_string = "".join(c for c in nfd_form if not unicodedata.combining(c))
+    return striped_string
 
 
-def split_id_card(id_card: str) -> IdCardParts:
-    if len(id_card) < 4:
-        raise ValueError("El id_card debe tener al menos 4 caracteres")
-
-    id_card_parts = IdCardParts(
-        plan=id_card[:2], contract=id_card[2:-2], family=id_card[-2], user=id_card[-1]
-    )
-    return dumps(asdict(id_card_parts))
-
-
-print(split_id_card("10632707112"))
+if __name__ == "__main__":
+    print(strip_acents("Holá que más"))
+    print(strip_acents("Hola que mas"))
