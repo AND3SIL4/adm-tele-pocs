098b4882e7b6ed9e65ff8af7c3bda8715e682028
AND3SIL4 <devaul.fs@gmail.com>
2026-03-27 09:57:55 -0500

    feat: analyse the structure and create the sentences of sqlite

--- database/queries-and-inserts.sql
+++ database/queries-and-inserts.sql
@@ -1,21 +1,21 @@
 -- Insert a new execution date
-INSERT INTO dates(exe_date)
-VALUES ('12/03/2026');
+INSERT OR IGNORE INTO dates(exe_date)
+VALUES ('13/03/2026');
 
 -- Insert a new record
-INSERT OR IGNORE INTO records(id_record, exe_date)
-VALUES ('100023494RESS', '12/03/2026');
+INSERT OR IGNORE INTO records(id_record, exe_date, state)
+VALUES ('52040290-programa-saludmental-medicinageneral-teleorientación', '13/03/2026', 1);
 
 -- Create a query to search if the record exists
 SELECT 1
 FROM records
-WHERE exe_date = '12/03/2026' 
+WHERE exe_date = '13/03/2026' 
 AND id_record = '100023494RESS';
 
 -- Select the date to know if there is any record
-SELECT 1
+SELECT exe_date
 FROM dates
-WHERE exe_date = '13/03/2026'
+ORDER BY start_date ASC;
 
 -- Validate if the tables were created
 SELECT 1
@@ -23,6 +23,19 @@ FROM sqlite_master
 WHERE type = 'table'
 AND name = 'dates'; -- table name
 
+-- Query to get the accuracy of the records
+SELECT COUNT(*)
+FROM records
+WHERE state = 1;
 
-SELECT *
-FROM sqlite_master;
+SELECT COUNT(*)
+FROM records;
+
+SELECT COUNT(*)
+FROM records
+WHERE state = 2;
+
+-- Delte the date record after finish validations
+DELETE
+FROM dates
+WHERE exe_date = '13/03/2026'
